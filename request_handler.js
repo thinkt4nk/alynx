7a5e99b98d535d2771b47e19101f491a1f89fcf8
Ryan Bales <ryanbales@Ryan-Baless-iMac.local>
2011-07-26 15:55:08 -0500

    added request method handling for POST method

--- request_handler.js
+++ request_handler.js
@@ -10,6 +10,11 @@ var ok = function(response,message)
 	}
 	response.end();
 };
+var redirect = function(response,destination)
+{
+	response.writeHead(303,{'Location' : destination});
+	response.end();
+}
 var getQuery = function(request)
 {
 	return querystring.parse(url.parse(request.url).query);
@@ -18,19 +23,23 @@ var getQuery = function(request)
 
 // HANDLERS
 var actions = {
-	status : function(request,response)
+	test_form : function(request,response)
 	{
-		var query = getQuery(request);
-		console.log(query);
-		ok(response,'status');
-	},
-	uploads : function(request,response)
-	{
-		var query = getQuery(request);
-		console.log(query);
-		ok(response,'uploads');
+		if( typeof(request.POST) !== 'undefined' )
+		{
+			console.log(request.POST);
+			redirect(response,'/test_form');
+		}
+		else
+		{
+			// output form
+			response.writeHead(200,{'Content-Type':'text/html'});
+			response.write('<html><head></head><body><form action="" method="POST"><label>name</label><input name="name" type="text" /><br /><label>age</label><input type="text" name="age" /><br /><input type="submit" value="Submit" /></form></body></html>');
+			response.end();
+		}
 	}
 };
+
 
 var handle = function(request,response)
 {
@@ -38,7 +47,26 @@ var handle = function(request,response)
 	if( typeof(actions[pathname]) === 'function' )
 	{
 		console.log('Routing to handler:'+ pathname);
-		actions[pathname](request,response);
+		// Handle and inject GET data into request object
+		var query = getQuery(request);
+		request['GET'] = query;
+		// Handle POST, if applicable
+		if( request.method === 'POST' )
+		{
+			var post_data = "";
+			request.addListener('data',function(chunk) {
+				post_data += chunk;
+			});
+			request.addListener('end',function() {
+				var post_object = querystring.parse(post_data);
+				request['POST'] = post_object;
+				actions[pathname](request,response);
+			});
+		}
+		else
+		{
+			actions[pathname](request,response);
+		}
 	}
 	else
 	{
--- server.js
+++ server.js
@@ -1,9 +1,11 @@
 var http = require('http');
 
+var server_port = 8888;
 var server = function(route) {
+	console.log('starting server on port '+ server_port);
 	http.createServer(function(request,response) {
 		route(request,response);
-	}).listen(8888);
+	}).listen(server_port);
 };
 
 exports.start = server;
